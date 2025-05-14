54017fcb33559f07fa8e0403563f8e1f2e0fdeb2
Achyuth27 <achyuthbitw@gmail.com>
2025-05-14 22:05:29 +0530

    Fix: use dynamic port for Render

--- app.py
+++ app.py
@@ -64,4 +64,6 @@ def predict():
 # Run Flask app
 if __name__ == "__main__":
     print("🔧 Server running on http://127.0.0.1:5000")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
